a77c610bf75d81e4759646386fb63ed29c7e2745
luk3mn <lucasrenan2015@gmail.com>
2024-01-08 18:55:17 -0300

    feat: content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Let's watch Naruto",
+        "description": "Naruto Naruto Naruto Naruto Naruto Naruto",
+        "photoCover":"https://facts.net/wp-content/uploads/2023/05/Naruto.jpeg"
+    },
+    {
+        "id":"2",
+        "photo":"bbb",
+        "title":"New content",
+        "description": "Hello",
+        "photoCover":""
+    }
+]
